88f22c37121d4c561e3a453ff7c322b4e5e488bd
yuan7122 <39150346+yuan7122@users.noreply.github.com>
2023-06-20 14:37:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let databaseURL = 'https://script.google.com/macros/s/AKfycbzNPpmMM-eZbaRnnOvh6n1OsYQefouXW0Lymd3L9OW6fJSlFZy2Xr7Nz20cX8K0Z8rs/exec';
+let databaseURL = 'https://script.google.com/macros/s/AKfycbxRknWkKvGXJ81CX0Hzo3G8v--GSTrkmHsl0ORBHoR5EbQbNB433s2yNylTu24ASHko/exec';
 
 // fetch raw database
 let rawProblemData = await fetch(databaseURL).then(res => res['json']());
