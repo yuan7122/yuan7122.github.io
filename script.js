922bcaccef8a62c467cacaf29b8c6711a70ba9b1
yuan7122 <39150346+yuan7122@users.noreply.github.com>
2023-06-20 14:55:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ let databaseURL = 'https://script.google.com/macros/s/AKfycbxRknWkKvGXJ81CX0Hzo3
 
 // fetch raw database
 let rawProblemData = await fetch(databaseURL).then(res => res['json']());
-function sendEmail() {
-  Email.send({
-    Host : "smtp.mailtrap.io",
-    To : rawProblemData[0]['email'],
-    From : "sender@example.com",
-    Subject : "Test email",
-    Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
-  }).then(
-    message => alert(message)
-  );
+function sendMail(name, email, subject, message) {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.set('Authorization', 'Basic ' + btoa('<API Key>'+":" +'<Secret Key>'));
+
+  const data = JSON.stringify({
+    "Messages": [{
+      "From": {"Email": "test@gmail.com", "Name": "test@gmail.com"},
+      "To": [{"Email": rawProblemData[0]['email'], "Name": 'test'}],
+      "Subject": subject,
+      "TextPart": message
+    }]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: data,
+  };
+
+  fetch("https://api.mailjet.com/v3.1/send", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 }
 console.log(rawProblemData)
